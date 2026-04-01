038e6c68f050f594634f38105ebb6cc798ec327b
Angel Benitez <angelbenitezramirez0@gmail.com>
2026-04-01 13:16:06 -0600

    feat: UI navbar agregado.

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,5 +2,14 @@
 </script>
 
 <template>
-
+    <nav class="flex justify-between px-12 py-6">
+        <span class="font-bold text-lg text-blue-900/80">Web<span class="text-black/80">Impulsa</span></span>
+        <div class="space-x-8 font-semibold text-sm">
+            <a href="#inicio">Inicio</a>
+            <a href="#problema">Problema</a>
+            <a href="#solucion">Solucion</a>
+            <a href="#servicios">Servicios</a>
+            <a href="#testimonios">Testimonios</a>
+        </div>
+    </nav>
 </template>
